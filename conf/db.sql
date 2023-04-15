da80c4829d2f7b09291122c7aa7652c4c6021fb2
Misterdev19 <jcpenagos716@outlook.com>
2023-04-15 15:47:10 -0500

    feat: :bug: tipo de dato de id_task_list

--- conf/db.sql
+++ conf/db.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE task_list(
-  id_task_list  PRIMARY KEY  AUTO_INCREMENT NOT NULL , 
+  id_task_list INT(11) PRIMARY KEY  AUTO_INCREMENT NOT NULL , 
   name_task_list VARCHAR(50),
   status_task_list CHAR(1) DEFAULT 1 
 );
